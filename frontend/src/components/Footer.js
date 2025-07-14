0e503afc27d512346e0b2f03671bea820e249494
manish <manishshrivastav423@gmail.com>
2025-07-14 12:54:32 +0530

    added readme

--- frontend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white text-center py-6 mt-10">
-        <p className="text-sm">© 2025 Maid For You. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
